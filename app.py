3c48020c7887f75081c126a8e9a1435c0b87fda4
Tieler Kayfes <100615112+duckguy1208@users.noreply.github.com>
2026-04-25 20:41:14 -0500

    patch 2.1

--- app.py
+++ app.py
@@ -1,24 +1,24 @@
 import streamlit as st
 import math
 
-def main():
-    st.title("Rpm To Speed Calculator for Mini Bikes")
 
-    # Inputs
-    f_ratio = st.number_input("Front Sprocket Teeth", value=12)
-    r_ratio = st.number_input("Rear Sprocket Teeth", value=75)
-    rpm = st.number_input("Engine RPM", value=3600)
-    tire_size = st.number_input("Tire Diameter (Inches)", value=12) # Standard mini bike tire
 
-    #calculate the gear ratio
-    ratio = r_ratio / f_ratio
-    #calculate the circumference of the tire
-    circumference = 3.1416 * tire_size
-    #calculate the speed in mph
-    speed = (circumference * ratio * 60) / (ratio * 63360)
+st.title("Rpm To Speed Calculator for Mini Bikes")
 
-    st.divider()
-    st.metric("Calculated Top Speed", f"{round(speed, 1)} MPH")
-    st.write(f"Your total gear ratio is **{round(ratio, 2)}:1**")
+# Inputs
+f_ratio = st.number_input("Front Sprocket Teeth", value=12)
+r_ratio = st.number_input("Rear Sprocket Teeth", value=75)
+rpm = st.number_input("Engine RPM", value=3600)
+tire_size = st.number_input("Tire Diameter (Inches)", value=12) # Standard mini bike tire
 
-main()
+#calculate the gear ratio
+ratio = r_ratio / f_ratio
+#calculate the circumference of the tire
+circumference = 3.1416 * tire_size
+#calculate the speed in mph
+speed = (circumference * ratio * 60) / (ratio * 63360)
+
+st.divider()
+st.metric("Calculated Top Speed", f"{round(speed, 1)} MPH")
+st.write(f"Your total gear ratio is **{round(ratio, 2)}:1**")
+
