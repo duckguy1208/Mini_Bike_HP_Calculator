6f5aac2144ce127ceae3a9a415c9155339267ec8
Tieler Kayfes <100615112+duckguy1208@users.noreply.github.com>
2026-04-25 21:30:27 -0500

    patch to app.py

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import streamlit as st
 import math
 
-st.title("Mini Bike Speed Calculator")
+st.title("Mini Bike Calculator")
 
 def speed_calculator():
 
     # Inputs
-    f_ratio = st.number_input("Front Sprocket Teeth (Teeth)", value=12)
-    r_ratio = st.number_input("Rear Sprocket Teeth (Teeth)", value=75)
-    rpm = st.number_input("Engine RPM", value=3600)
-    tire_size = st.number_input("Tire Diameter (Inches)", value=14) 
+    f_ratio = st.number_input("Front Sprocket Teeth (Teeth)", value=12,  key="input_one")
+    r_ratio = st.number_input("Rear Sprocket Teeth (Teeth)", value=75, key="input_two")
+    rpm = st.number_input("Engine RPM", value=3600, key="input_three")
+    tire_size = st.number_input("Tire Diameter (Inches)", value=14, key="input_four")
 
     #calculate the gear ratio
     ratio = r_ratio / f_ratio
@@ -19,15 +19,15 @@ def speed_calculator():
     speed = (rpm * circumference * 60) / (ratio * 63360)
 
     st.divider()
-    st.metric("Calculated Top Speed", f"{round(speed_calculator(), 1)} MPH")
+    st.metric("Calculated Top Speed", f"{round(speed, 1)} MPH")
 
 def rpm_calculator():
 
     # inputs
-    f_ratio = st.number_input("Front Sprocket Teeth (Teeth)", value=12)
-    r_ratio = st.number_input("Rear Sprocket Teeth (Teeth)", value=75)
-    speed = st.number_input("Speed (MPH)", value=20)
-    tire_size = st.number_input("Tire Diameter (Inches)", value=14)
+    speed = st.number_input("Speed (MPH)", value=20, key="input_five")
+    f_ratio = st.number_input("Front Sprocket Teeth (Teeth)", value=12, key="input_six")
+    r_ratio = st.number_input("Rear Sprocket Teeth (Teeth)", value=75, key="input_seven")
+    tire_size = st.number_input("Tire Diameter (Inches)", value=14, key="input_eight")
 
     # calculate the gear ratio
     ratio = r_ratio / f_ratio
@@ -42,8 +42,8 @@ def rpm_calculator():
 def gear_ratio_calculator():
 
     # inputs
-    f_ratio = st.number_input("Front Sprocket Teeth (Teeth)", value=12)
-    r_ratio = st.number_input("Rear Sprocket Teeth (Teeth)", value=75)
+    f_ratio = st.number_input("Front Sprocket Teeth (Teeth)", value=12, key="input_nine")
+    r_ratio = st.number_input("Rear Sprocket Teeth (Teeth)", value=75, key="input_ten")
 
     # calculate the gear ratio
     ratio = r_ratio / f_ratio
