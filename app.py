188c15132f82e88e2161b8cb23b279434fa5ab48
Tieler Kayfes <100615112+duckguy1208@users.noreply.github.com>
2026-04-25 20:40:01 -0500

    patch 2.0

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import streamlit as st
+import math
 
 def main():
     st.title("Rpm To Speed Calculator for Mini Bikes")
 
     # Inputs
-    f_ratio = st.number_input("Front Sprocket Teeth", value=15)
-    r_ratio = st.number_input("Rear Sprocket Teeth", value=40)
-    rpm = st.number_input("Engine RPM", value=5000)
-    tire_size = st.number_input("Tire Diameter (Inches)", value=14.5) # Standard mini bike tire
+    f_ratio = st.number_input("Front Sprocket Teeth", value=12)
+    r_ratio = st.number_input("Rear Sprocket Teeth", value=75)
+    rpm = st.number_input("Engine RPM", value=3600)
+    tire_size = st.number_input("Tire Diameter (Inches)", value=12) # Standard mini bike tire
 
     #calculate the gear ratio
     ratio = r_ratio / f_ratio
