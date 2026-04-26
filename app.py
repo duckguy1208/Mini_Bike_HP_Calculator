590b57be9b1d9297161121a691bb7003d18e98ea
Tieler Kayfes <100615112+duckguy1208@users.noreply.github.com>
2026-04-25 21:24:09 -0500

    added selectable calculator options and implemented the gear ratio calculator

--- app.py
+++ app.py
@@ -3,26 +3,62 @@ import math
 
 st.title("Mini Bike Speed Calculator")
 
-col1, col2 = st.columns(2)
-with col1:
-    num1 = st.number_input("Input A")
-with col2:
-    num2 = st.number_input("Input B")
+def speed_calculator():
 
-# Inputs
-f_ratio = st.number_input("Front Sprocket Teeth (Teeth)", value=12)
-r_ratio = st.number_input("Rear Sprocket Teeth (Teeth)", value=75)
-rpm = st.number_input("Engine RPM", value=3600)
-tire_size = st.number_input("Tire Diameter (Inches)", value=14) 
+    # Inputs
+    f_ratio = st.number_input("Front Sprocket Teeth (Teeth)", value=12)
+    r_ratio = st.number_input("Rear Sprocket Teeth (Teeth)", value=75)
+    rpm = st.number_input("Engine RPM", value=3600)
+    tire_size = st.number_input("Tire Diameter (Inches)", value=14) 
 
-#calculate the gear ratio
-ratio = r_ratio / f_ratio
-#calculate the circumference of the tire
-circumference = 3.1416 * tire_size
-#calculate the speed in mph
-speed = (rpm * circumference * 60) / (ratio * 63360)
+    #calculate the gear ratio
+    ratio = r_ratio / f_ratio
+    #calculate the circumference of the tire
+    circumference = 3.1416 * tire_size
+    #calculate the speed in mph
+    speed = (rpm * circumference * 60) / (ratio * 63360)
 
-st.divider()
-st.metric("Calculated Top Speed", f"{round(speed, 1)} MPH")
+    st.divider()
+    st.metric("Calculated Top Speed", f"{round(speed_calculator(), 1)} MPH")
+
+def rpm_calculator():
+
+    # inputs
+    f_ratio = st.number_input("Front Sprocket Teeth (Teeth)", value=12)
+    r_ratio = st.number_input("Rear Sprocket Teeth (Teeth)", value=75)
+    speed = st.number_input("Speed (MPH)", value=20)
+    tire_size = st.number_input("Tire Diameter (Inches)", value=14)
+
+    # calculate the gear ratio
+    ratio = r_ratio / f_ratio
+    # calculate the circumference of the tire
+    circumference = 3.1416 * tire_size
+    # calculate the engine RPM
+    rpm = (speed * ratio * 63360) / (circumference * 60)
+
+    st.divider()
+    st.metric("Engine RPM At Given Speed", f"{round(rpm, 1)} RPM")
+
+def gear_ratio_calculator():
+
+    # inputs
+    f_ratio = st.number_input("Front Sprocket Teeth (Teeth)", value=12)
+    r_ratio = st.number_input("Rear Sprocket Teeth (Teeth)", value=75)
+
+    # calculate the gear ratio
+    ratio = r_ratio / f_ratio
+    st.divider()
+    st.metric("Gear Ratio", f"{round(ratio, 2)} : 1")
+
+calculator = st.radio("Select Calculator", ["Speed", "RPM", "Gear Ratio"])
+
+if calculator == "Speed":
+    speed_calculator()
+elif calculator == "RPM":
+    rpm_calculator()
+else:
+    gear_ratio_calculator()
+
+
 
 
